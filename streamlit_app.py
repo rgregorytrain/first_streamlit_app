cb85542c500cf06f02002b977b6e33a6eedad5e1
rgregorytrain <115638263+rgregorytrain@users.noreply.github.com>
2022-10-12 15:13:51 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,9 +44,7 @@ my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contins:")
 streamlit.dataframe(my_data_row)
 
-fruit_choice2 = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice2)
-#add_my_fruit = streamlit.text("Hi")
-#add_my_fruit = streamlit.text_input('What fruit would you like information about?','Kiwi')
-#my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values (" + add_my_fruit + ")")
-streamlit.write('Thanks for adding ', add_my_fruit)
+#New Section to display fruityvice api respose
+streamlit.header('Fruity Frui Advice!')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
